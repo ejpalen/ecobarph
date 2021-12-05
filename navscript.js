76ebffc778845b09379fcdd2e66ffc58d0c9d2c9
Ej Palen <92970626+ejpalen@users.noreply.github.com>
2021-12-05 21:21:59 +0800

    Update navscript.js

--- navscript.js
+++ navscript.js
@@ -1,6 +1,12 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const toggle_1 = document.getElementsByClassName("toggle1")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const navbarLinks_1 = document.getElementsByClassName("navbar-links1")[0];
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active1");
+});
+
+toggle_1.addEventListener("click", () => {
+  navbarLinks_1.classList.toggle("active2");
+});
