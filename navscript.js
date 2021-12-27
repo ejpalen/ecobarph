166baac5c0a5fdfc7f3848a0c4349cda14471351
Ej Palen <92970626+ejpalen@users.noreply.github.com>
2021-12-27 14:59:28 +0800

    Update navscript.js

--- navscript.js
+++ navscript.js
@@ -10,3 +10,50 @@ toggleButton.addEventListener("click", () => {
 toggle_1.addEventListener("click", () => {
   navbarLinks_1.classList.toggle("active2");
 });
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active1");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active1");
+  overlay.classList.add("active1");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active1");
+  overlay.classList.remove("active1");
+}
+
+function GFG_click() {
+  var gfg_down = document.getElementById("gfg_down");
+  gfg_down.remove();
+}
+
+function hideOrShow() {
+  var x = document.getElementById("gfg_down");
+  x.style.display = "block";
+}
