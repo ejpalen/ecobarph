e56ad3bab591e24e96962df85cb0e4b8bcedaeb5
Ej Palen <92970626+ejpalen@users.noreply.github.com>
2022-02-02 15:12:55 +0800

    Update navscript.js

--- navscript.js
+++ navscript.js
@@ -58,7 +58,4 @@ function hideOrShow() {
   x.style.display = "block";
 }
 
-var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
-if (a.indexOf('html') > -1) { //Check of html String in URL.
-   url = url.substring(0, newURL.lastIndexOf("."));
-}
+
